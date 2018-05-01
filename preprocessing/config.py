6de744cd253f37cd96b0cd231fa4b4b421fe2d17
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-01 14:49:19 +0900

    edit

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -4,7 +4,7 @@ class Config():
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     last1 = run_dir_path.split("/")[-1]
     last2 = run_dir_path.split("/")[-1]
-    up_dir = run_dir_path.replace(last,"")
-    up_dir = up_dir.replace(last+"/","")
+    up_dir = run_dir_path.replace(last1,"")
+    up_dir = up_dir.replace(last2+"/","")
     image_dir_path = up_dir + "string2image/image"
 
