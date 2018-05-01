2fa64f97bbff88a685d3f6134ba366baafe11703
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-01 14:33:53 +0900

    もうひとつ上のディレクトリを見る

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -4,5 +4,7 @@ class Config():
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     last = run_dir_path.split("/")[-1]
     up_dir = run_dir_path.replace(last,"")
+    last = run_dir_path.split("/")[-1]
+    up_dir = run_dir_path.replace(last,"")
     image_dir_path = up_dir + "aozora_data/files/tmp.txt"
 
