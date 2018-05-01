78178ccfb04c84f6f7dba3ce7d1ce8e60c272a03
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-01 14:56:05 +0900

    画像ファイルないのファイルをいちおう拡張子pngで間引く

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -19,15 +19,16 @@ class PreProcessing(abc_preprocessing.ABCPreProcessing):
         image_list = []
         file_dir = config.Config.image_dir_path
         for file in os.listdir(file_dir):
-            filepath = file_dir + "/" + file
-            image = np.array(Image.open(filepath).resize((25, 25)))
-            print(filepath)
-            # 配列を変換し、[[Redの配列],[Greenの配列],[Blueの配列]] のような形にする。
-            image = image.transpose(2, 0, 1)
-            # さらにフラットな1次元配列に変換。最初の1/3はRed、次がGreenの、最後がBlueの要素がフラットに並ぶ。
-            image = image.reshape(1, image.shape[0] * image.shape[1] * image.shape[2]).astype("float32")[0]
-            # 出来上がった配列をimage_listに追加。
-            image_list.append(image / 255.)
+            if (file.split(".")[-1] == "png") :
+                filepath = file_dir + "/" + file
+                image = np.array(Image.open(filepath).resize((25, 25)))
+                print(filepath)
+                # 配列を変換し、[[Redの配列],[Greenの配列],[Blueの配列]] のような形にする。
+                image = image.transpose(2, 0, 1)
+                # さらにフラットな1次元配列に変換。最初の1/3はRed、次がGreenの、最後がBlueの要素がフラットに並ぶ。
+                image = image.reshape(1, image.shape[0] * image.shape[1] * image.shape[2]).astype("float32")[0]
+                # 出来上がった配列をimage_listに追加。
+                image_list.append(image / 255.)
         image_list = np.array(image_list)
         print(image_list.shape())
         return image_list, image_list
