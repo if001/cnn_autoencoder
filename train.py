de95e16df306795d9015c1e88329880eee2aa5e1
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-02 22:14:30 +0900

    修正

--- train.py
+++ train.py
@@ -6,11 +6,10 @@ from model.simple_autoencoder import SimpleAutoencoder
 from model_exec.config import Config
 
 def main():
-
+    autoencoder = SimpleAutoencoder.make_model()
     for i in range(10000):
         print("step: ",i)
         train_x, train_y = PreProcessing().make_train_data(Config.batch_size)
-        autoencoder = SimpleAutoencoder.make_model()
         hist = Learning.run(autoencoder, train_x, train_y)
         if i % 50 == 0:
             SimpleAutoencoder.save_model(autoencoder ,"autoencoder.hdf5")
