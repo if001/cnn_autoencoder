6e24450509dc70d4d75484eefab09d028164aec9
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-04 23:04:49 +0900

    resume修正

--- train-resume.py
+++ train-resume.py
@@ -8,9 +8,13 @@ def main():
     train_x, train_y = PreProcessing().make_train_data(Config.batch_size)
     autoencoder = SimpleAutoencoder.load_model("autoencoder.hdf5")
 
-    hist = Learning.run(autoencoder, train_x, train_y)
+    for i in range(10000):
+        print("step: ",i)
+        train_x, train_y = PreProcessing().make_train_data(Config.batch_size)
+        hist = Learning.run(autoencoder, train_x, train_y)
+        if i % 50 == 0:
+            SimpleAutoencoder.save_model(autoencoder ,"autoencoder.hdf5")
 
-    SimpleAutoencoder.save_model(autoencoder ,"autoencoder.hdf5")
 
 if __name__ == '__main__':
    main()
