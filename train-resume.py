1fab644dca78fdec0727b5915949e485bee8e07c
if <otomijuf.002@gmail.com>
2018-05-07 23:43:29 +0900

    edit resume

--- train-resume.py
+++ train-resume.py
@@ -6,25 +6,20 @@ from model.simple_autoencoder import SimpleAutoencoder
 
 import sys
 
-date_size = 60000
+date_size = 20000
 test_size = 10000
 
 
 def main():
     train_x, train_y = PreProcessing().make_train_data(Config.batch_size)
+    cbs = SimpleAutoencoder.set_callbacks("autoencoder.hdf5")
     autoencoder = SimpleAutoencoder.load_model("autoencoder.hdf5")
-
-    start = 0
-    if "i==" in sys.argv[-1]:
-        start = sys.argv[-1].split("==")[-1]
-
-    for i in range(10):
-        print("step: ", i)
-        train_x, train_y = PreProcessing().make_train_data(date_size)
-        test_x, test_y = PreProcessing().make_train_data(test_size)
-        hist = Learning.run_with_test(
-            autoencoder, train_x, train_y, test_x, test_y)
-        # SimpleAutoencoder.save_model(autoencoder, "autoencoder.hdf5")
+    
+    train_x, train_y = PreProcessing().make_train_data(date_size)
+    test_x, test_y = PreProcessing().make_train_data(test_size)
+    hist = Learning.run_with_test(
+        autoencoder, train_x, train_y, test_x, test_y, cbs)
+    # SimpleAutoencoder.save_model(autoencoder, "autoencoder.hdf5")
 
 
 if __name__ == '__main__':
