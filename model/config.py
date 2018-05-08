789b2936690dae458e8cab0355fb42c08b8fe358
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-08 17:54:39 +0900

    configのパラメタを変えてみる

--- model/config.py
+++ model/config.py
@@ -1,12 +1,13 @@
 from keras.optimizers import RMSprop
 import os
-
+import keras.optimizers.Adam
 
 class Config():
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_model = run_dir_path + "/weight/model.hdf5"
-    loss = 'mean_squared_error'
     loss = 'binary_crossentropy'
+    loss = 'mean_squared_error'
     optimizer = 'adadelta'
     optimizer = 'adam'
+    optimizer = Adam(lr=2e-4, beta_1=0.5)
     metrics = 'accuracy'
