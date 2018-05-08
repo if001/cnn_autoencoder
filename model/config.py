9d4be511e3a59bef54c172147a033457e3a043cf
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-08 14:49:15 +0900

    損失関数と最適化関数を修正

--- model/config.py
+++ model/config.py
@@ -6,5 +6,7 @@ class Config():
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_model = run_dir_path + "/weight/model.hdf5"
     loss = 'binary_crossentropy'
+    loss = 'mean_squared_error'
     optimizer = 'adadelta'
+    optimizer = 'adam'
     metrics = 'accuracy'
