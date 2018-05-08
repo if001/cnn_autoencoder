fc7c3278404efe35867ae5c6c865e33d72864e22
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-08 17:14:30 +0900

    損失関数を修正

--- model/config.py
+++ model/config.py
@@ -5,8 +5,8 @@ import os
 class Config():
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_model = run_dir_path + "/weight/model.hdf5"
-    loss = 'binary_crossentropy'
     loss = 'mean_squared_error'
+    loss = 'binary_crossentropy'
     optimizer = 'adadelta'
     optimizer = 'adam'
     metrics = 'accuracy'
